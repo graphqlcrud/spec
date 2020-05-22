e62c0221c11d6c86649a8c61268846a16601ccc6
Wojciech Trocki <wtrocki@redhat.com>
2020-05-22 17:42:26 +0100

    fix: remove versioning as temp solution

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,112 +1,114 @@
 /* eslint-disable */
-const versions = require('./versions.json');
+const versions = require("./versions.json");
 
 module.exports = {
-  title: 'GraphQLCRUD',
-  tagline: 'Specification that extends GraphQL with common data access use cases',
-  url: 'https://graphqlcrud.org',
-  baseUrl: '/',
-  favicon: 'img/logo.png',
+  title: "GraphQLCRUD",
+  tagline:
+    "Specification that extends GraphQL with common data access use cases",
+  url: "https://graphqlcrud.org",
+  baseUrl: "/",
+  favicon: "img/logo.png",
 
-  organizationName: 'GraphQLCRUD', // Usually your GitHub org/user name.
-  projectName: 'spec', // Usually your repo name.
+  organizationName: "GraphQLCRUD", // Usually your GitHub org/user name.
+  projectName: "spec", // Usually your repo name.
 
   themeConfig: {
     disableDarkMode: true,
     prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      defaultLanguage: 'javascript',
+      theme: require("prism-react-renderer/themes/github"),
+      defaultLanguage: "javascript",
     },
     navbar: {
-      title: 'GraphQLCRUD',
+      title: "GraphQLCRUD",
       logo: {
-        alt: 'GraphQLCRUD Logo',
-        src: 'img/logo.png',
+        alt: "GraphQLCRUD Logo",
+        src: "img/logo.png",
       },
       links: [
         {
-          to: 'docs/gettingstarted',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-          items: [
-            // Disable temporarily until we have  version of the spec
-            // {
-            //   label: versions[0],
-            //   to: 'docs/getting-started',
-            // },
-            ...versions.slice(1).map((version) => ({
-              label: version,
-              to: `docs/${version}/getting-started`,
-            })),
-            {
-              label: 'Master/Unreleased',
-              to: 'docs/next/getting-started',
-            },
-          ],
+          to: "docs/gettingstarted",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+          to: "docs/getting-started",
+          // items: [
+          // Disable temporarily until we have  version of the spec
+          // {
+          //   label: versions[0],
+          //   to: 'docs/getting-started',
+          // },
+          // ...versions.slice(1).map((version) => ({
+          //   label: version,
+          //   to: `docs/${version}/getting-started`,
+          // })),
+          //   {
+          //     label: 'Master/Unreleased',
+          //     to: 'docs/next/getting-started',
+          //   },
+          // ],
         },
+        // {
+        //   to: 'versions',
+        //   label: `v${versions[0]}`,
+        //   position: 'right',
+        // },
         {
-          to: 'versions',
-          label: `v${versions[0]}`,
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/GraphQLCRUD/spec',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/GraphQLCRUD/spec",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: 'docs/getting-started',
+              label: "Getting Started",
+              to: "docs/getting-started",
             },
             {
-              label: 'Releases',
-              to: 'docs/release-notes',
-            }
+              label: "Releases",
+              to: "docs/release-notes",
+            },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/GraphQLCRUD/spec',
+              label: "GitHub",
+              href: "https://github.com/GraphQLCRUD/spec",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/mJ7j84m',
+              label: "Discord",
+              href: "https://discordapp.com/invite/mJ7j84m",
             },
           ],
         },
       ],
       logo: {
-        alt: 'AeroGear Logo',
-        src: 'img/aerogear.png',
-        href: 'https://aerogear.org/',
+        alt: "AeroGear Logo",
+        src: "img/aerogear.png",
+        href: "https://aerogear.org/",
       },
       copyright: `Copyright © ${new Date().getFullYear()} AeroGear`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          path: '../docs',
-          routeBasePath: 'docs',
-          sidebarPath: require.resolve('./sidebars.json'),
+          path: "../docs",
+          routeBasePath: "docs",
+          sidebarPath: require.resolve("./sidebars.json"),
           editUrl:
-            'https://github.com/aerogear/GraphQLCRUD/edit/master/website/',
+            "https://github.com/aerogear/GraphQLCRUD/edit/master/website/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
